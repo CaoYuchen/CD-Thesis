2a24f020b49c451baf63db5c3831e8926e91896e
Joshua Rain <1014054551@qq.com>
2022-03-27 16:51:26 -0400

    paper reading log update

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $('#text-gt').click(function() {
 $('#text-code').click(function() {
   $('#navigation a[href="#code"]').tab('show');
 });
+
+$('.tag').click(function() {
+  $(".DL, .CV, .HCI, .CG").hide();
+  console.log($(this));
+  if($(this).hasClass("tag-DL")){
+    $(".DL").show();
+  }
+  if($(this).hasClass("tag-CG")){
+    $(".CG").show();
+  }
+  if($(this).hasClass("tag-CV")){
+    $(".CV").show();
+  }
+  if($(this).hasClass("tag-HCI")){
+    $(".HCI").show();
+  }
+  if($(this).hasClass("tag-ALL")){
+    $(".DL, .CV, .HCI, .CG").show();
+  }
+});
+
 })
